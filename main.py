e15723e22d6456c3296f4e02e8fdc7d687382183
CAngell0 <carsonjesseangell.learn@gmail.com>
2026-04-10 13:46:43 -0600

    Successfully made database connection, used engineering db for testing

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import os
 from dotenv import load_dotenv
+import mysql.connector
 
 load_dotenv();
 
-test = os.getenv('TEST_VALUE')
+db_connection = mysql.connector.connect(
+    host = os.getenv('DB_HOST'),
+    user = os.getenv('DB_USER'),
+    password = os.getenv('DB_PASSWORD'),
+    database = os.getenv('DB_DATABASE')
+);
 
-print(test)
+if db_connection.is_connected():
+    print("Successfully connected to the database")
 
+cursor = db_connection.cursor()
+
+cursor.execute('SHOW TABLES;')
+for row in cursor.fetchall():
+    print(row)
