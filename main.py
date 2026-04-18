89b1bc1c67068fc3360a1ccc6806670443536cc5
Carson Angell <carsonjesseangell@gmail.com>
2026-04-18 13:01:37 -0600

    Added all the utah regions shown on the region map

--- main.py
+++ main.py
@@ -44,7 +44,21 @@ api = RentCastAPI(
 
 # Configured regions for the script to target
 TARGET_REGIONS = [
-    APISearchRegion('UT', 40.634901, -111.917321, 15)
+    APISearchRegion('UT', 40.634901, -111.917321, 15), # Salt Lake City Region
+    APISearchRegion('UT', 41.055363, -111.958598, 18), # Davis County Region
+    APISearchRegion('UT', 41.476743, -112.124352, 18), # Box Elder County Region
+    APISearchRegion('UT', 41.847091, -112.047417, 18), # Cache County Region
+    APISearchRegion('UT', 40.237791, -111.754837, 18), # Utah County Region #1
+    APISearchRegion('UT', 39.962990, -112.099077, 10), # Utah County Region #2
+    APISearchRegion('UT', 39.889470, -111.725948, 10), # Utah County Region #3
+    APISearchRegion('UT', 41.533898, -111.430000, 30), # Rich County Region (Uinta Mountains)
+    APISearchRegion('UT', 40.842864, -111.242393, 30), # Summit County Region
+    APISearchRegion('UT', 40.033250, -110.105717, 80), # Duchesne County Region
+    APISearchRegion('UT', 37.985132, -110.373004, 80), # Garfield County Region
+    APISearchRegion('UT', 37.812867, -112.968922, 40), # Iron County Region
+    APISearchRegion('UT', 37.159613, -113.492389, 18), # Washington County Region (St.George)
+    APISearchRegion('UT', 39.046094, -112.355125, 60), # Millard County Region
+    APISearchRegion('UT', 40.390796, -113.222394, 60), # Tooele County Region
 ]
 
 
